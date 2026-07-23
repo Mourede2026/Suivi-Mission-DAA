d370c655dafc73bdab2368eccad87b602f7ed0bc
Mourede2026 <sossouhounto_2000@hotmail.com>
2026-07-23 20:07:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // (Extensions > Apps Script > Déployer > Nouveau déploiement > Application Web)
 // Elle ressemble à : https://script.google.com/macros/s/AKfycb.../exec
 // ===========================================================================
-const WEBAPP_URL = "COLLEZ_ICI_VOTRE_URL_DE_DEPLOIEMENT_APPS_SCRIPT";
+const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbzEtD4jYmNyqh3YUuqsZkHSZ5L9zKLHDFInszoXYbwoAGSI5CVeEuVnmqiK8i32JrYwGQ/exec";
