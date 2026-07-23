cbd5e927416534140aab87fe6c971a89b9faadd2
Mourede2026 <sossouhounto_2000@hotmail.com>
2026-07-23 07:57:46 +0100

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// ===========================================================================
-// À MODIFIER : collez ici l'URL de votre déploiement Apps Script
-// (Extensions > Apps Script > Déployer > Nouveau déploiement > Application Web)
-// Elle ressemble à : https://script.google.com/macros/s/AKfycb.../exec
-// ===========================================================================
-const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbw6w-Tr5bS1dDNZ4j7lX8u1VqxadkdDJ7zQpa-bgAiyIV38ClWq-SslVNwhTX2fn2VnMg/exec";
