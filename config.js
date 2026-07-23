5df104888487f68d42845fb822596edc7d1e284d
Mourede2026 <sossouhounto_2000@hotmail.com>
2026-07-23 20:07:09 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// ===========================================================================
+// À MODIFIER : collez ici l'URL de votre déploiement Apps Script
+// (Extensions > Apps Script > Déployer > Nouveau déploiement > Application Web)
+// Elle ressemble à : https://script.google.com/macros/s/AKfycb.../exec
+// ===========================================================================
+const WEBAPP_URL = "COLLEZ_ICI_VOTRE_URL_DE_DEPLOIEMENT_APPS_SCRIPT";
